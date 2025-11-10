c00d6ae45dd64a137dfd6469925bcec8ca67bd21
pswon5894 <pswon5894@gmail.com>
2025-11-10 18:59:42 +0900

    위치 권한 기능

--- app/src/main/java/com/cc/air_quality/MainActivity.kt
+++ app/src/main/java/com/cc/air_quality/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.cc.air_quality
 
+import android.Manifest
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cc.air_quality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    val REQUIRED_PERMISSIONS =arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    lateinit var getGPSPermissionLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        checkAllPermissions()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun checkAllPermissions() {
+        if(!isLocationServiceAvailable()){
+            showDialogForLocaionServiceSetting()
+        } else{
+            isRunTimePermissionsGranted()
+        }
+    }
+
+    private fun isLocationServiceAvailable() :Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER))
     }
 }
